686008fe9705b406fd26a3e19a559171ae353635
Ivan Ristović <ivo.ristovic@gmail.com>
2020-12-14 23:23:37 +0100

    [ANTLR] Add Java ANTLR files; Create templates

---
+++ LINVAST.Imperative/Builders/Java/JavaASTBuilder.Types.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using LINVAST.Builders;
+using LINVAST.Exceptions;
+using LINVAST.Imperative.Nodes;
+using LINVAST.Logging;
+using LINVAST.Nodes;
+using static LINVAST.Imperative.Builders.Java.JavaParser;
+
+namespace LINVAST.Imperative.Builders.Java
+{
+    public sealed partial class JavaASTBuilder : JavaBaseVisitor<ASTNode>, IASTBuilder<JavaParser>
+    {
+        // TODO after types are added to AST
+    }
+}
