12168f7702c5b91a72e20660c5e0e4a4cedc1a1c
marijakatic <katic.marija.97@gmail.com>
2021-08-04 20:03:35 +0200

    Class, enum and interface declarations Java AST Builder; Tests

--- LINVAST.Imperative/Builders/Java/JavaASTBuilder.Declarations.cs
+++ LINVAST.Imperative/Builders/Java/JavaASTBuilder.Declarations.cs
@@ -31,7 +31,190 @@ namespace LINVAST.Imperative.Builders.Java
             return new ImportNode(ctx.Start.Line, directive.ToString());
         }
 
-        
+
+        // class, enum, interface declarations:
+
+        public override ASTNode VisitClassDeclaration([NotNull] ClassDeclarationContext ctx)
+        {
+            var identifier = new IdNode(ctx.Start.Line, ctx.IDENTIFIER().GetText());
+
+            TypeNameListNode templateParams;
+            if (ctx.typeParameters() is { } typeParamsCtx)
+                templateParams = this.Visit(typeParamsCtx).As<TypeNameListNode>();
+            else
+                templateParams = new TypeNameListNode(ctx.Start.Line, new TypeNameNode[] { });
+
+            IEnumerable<TypeNameNode> baseTypes;
+            int baseTypesStartLine = ctx.Start.Line;
+            if (ctx.typeList() is { } typeListCtx) {
+                baseTypes = this.Visit(typeListCtx).As<TypeNameListNode>().Types;
+                baseTypesStartLine = typeListCtx.Start.Line;
+            }
+            else baseTypes = new TypeNameNode[] { };
+            if (ctx.typeType() is { } typeTypeCtx) {
+                baseTypes = baseTypes.Append(this.Visit(typeTypeCtx).As<TypeNameNode>());
+                baseTypesStartLine = typeTypeCtx.Start.Line;
+            }
+
+            IEnumerable<DeclStatNode> declarations;
+            BlockStatNode block = this.Visit(ctx.classBody()).As<BlockStatNode>();
+            declarations = block.Children.Select(c => c.As<DeclStatNode>());
+
+            return new TypeDeclNode(ctx.Start.Line, identifier, templateParams,
+                new TypeNameListNode(baseTypesStartLine, baseTypes),
+                declarations);
+        }
+
+        public override ASTNode VisitEnumDeclaration([NotNull] EnumDeclarationContext ctx)
+        {
+            var identifier = new IdNode(ctx.Start.Line, ctx.IDENTIFIER().GetText());
+
+            if (ctx.typeList() is { })
+                throw new NotImplementedException("enum implements interface");
+
+            IEnumerable<VarDeclNode> constants = new VarDeclNode[] { };
+            if (ctx.enumConstants() is { }) {
+                BlockStatNode block = this.Visit(ctx.enumConstants()).As<BlockStatNode>();
+                constants = block.Children.Select(c => c.As<VarDeclNode>());
+            }
+
+            if (ctx.enumBodyDeclarations() is { })
+                throw new NotImplementedException("enum class body");
+
+            return new EnumDeclNode(ctx.Start.Line, identifier, constants);
+        }
+
+        public override ASTNode VisitEnumBodyDeclarations([NotNull] EnumBodyDeclarationsContext ctx)
+            => throw new NotImplementedException("Enum class body");
+
+        public override ASTNode VisitInterfaceDeclaration([NotNull] InterfaceDeclarationContext ctx)
+        {
+            var identifier = new IdNode(ctx.Start.Line, ctx.IDENTIFIER().GetText());
+
+            TypeNameListNode? templateParams = null;
+            if (ctx.typeParameters() is { })
+                templateParams = this.Visit(ctx.typeParameters()).As<TypeNameListNode>();
+
+            TypeNameListNode? baseTypes = null;
+            if (ctx.typeList() is { })
+                baseTypes = this.Visit(ctx.typeList()).As<TypeNameListNode>();
+
+            IEnumerable<DeclStatNode> declarations;
+            BlockStatNode block = this.Visit(ctx.interfaceBody()).As<BlockStatNode>();
+            declarations = block.Children.Select(c => c.As<DeclStatNode>());
+
+            return new TypeDeclNode(ctx.Start.Line, identifier,
+                templateParams ?? new TypeNameListNode(ctx.Start.Line),
+                baseTypes ?? new TypeNameListNode(ctx.Start.Line),
+                declarations);
+        }
+
+        public override ASTNode VisitClassBodyDeclaration([NotNull] ClassBodyDeclarationContext ctx)
+        {
+            if (ctx.SEMI() is { })
+                return new EmptyStatNode(ctx.Start.Line);
+
+            if (ctx.STATIC() is { } || ctx.block() is { })
+                throw new NotImplementedException("static- and non-static- blocks in a class");
+
+
+            // we use private method ProcessModifier instead of overriding VisitModifier
+            string modifiers = "";
+            int? declSpecsStartLine = null;
+            if (ctx.modifier() is { } modifierCtxList && modifierCtxList.Any()) {
+                modifiers = string.Join(" ", modifierCtxList.Select(modCtx => this.ProcessModifier(modCtx)));
+                declSpecsStartLine = modifierCtxList.First().Start.Line;
+            }
+
+            MemberDeclarationContext memberDeclCtx = ctx.memberDeclaration();
+
+            DeclSpecsNode declSpecs;
+            TypeNameNode? typeName = TypeName(memberDeclCtx);
+            if (typeName is { }) { // if memberDeclaration is anything but constructor- or genericConstructor- Declaration
+                declSpecsStartLine ??= typeName.Line;
+                declSpecs = new DeclSpecsNode(declSpecsStartLine ?? ctx.Start.Line, modifiers, typeName);
+            }
+            else // if memberDeclaration is constructor- or genericConstructor- Declaration
+                throw new NotImplementedException("constructors");
+
+            DeclListNode declList;
+            if (memberDeclCtx.fieldDeclaration() is { })
+                declList = this.Visit(memberDeclCtx).As<DeclListNode>();
+            else
+                declList = new DeclListNode(memberDeclCtx.Start.Line, this.Visit(memberDeclCtx).As<DeclNode>());
+
+            return new DeclStatNode(ctx.Start.Line, declSpecs, declList);
+
+            TypeNameNode? TypeName([NotNull] MemberDeclarationContext ctx)
+            {
+                if (ctx.classDeclaration() is { } clsDeclCtx)
+                    return new TypeNameNode(clsDeclCtx.Start.Line, clsDeclCtx.IDENTIFIER().GetText());
+
+                if (ctx.interfaceDeclaration() is { } interfaceDeclCtx)
+                    return new TypeNameNode(interfaceDeclCtx.Start.Line, interfaceDeclCtx.IDENTIFIER().GetText());
+
+                if (ctx.enumDeclaration() is { } enumDeclCtx)
+                    return new TypeNameNode(enumDeclCtx.Start.Line, enumDeclCtx.IDENTIFIER().GetText());
+
+                if (ctx.annotationTypeDeclaration() is { })
+                    throw new NotImplementedException("annotation type declaration");
+
+                if (ctx.methodDeclaration() is { } methodDeclCtx)
+                    return this.Visit(methodDeclCtx.typeTypeOrVoid()).As<TypeNameNode>();
+
+                if (ctx.genericMethodDeclaration() is { } genMethDeclCtx)
+                    return this.Visit(genMethDeclCtx.methodDeclaration().typeTypeOrVoid()).As<TypeNameNode>();
+
+                if (ctx.fieldDeclaration() is { } fieldDeclCtx)
+                    return this.Visit(fieldDeclCtx.typeType()).As<TypeNameNode>();
+
+                if (ctx.constructorDeclaration() is { })
+                    return null;
+
+                if (ctx.genericConstructorDeclaration() is { })
+                    return null;
+
+                // unreachable path
+                throw new SyntaxErrorException("Source file contained unexpected content");
+            }
+        }
+
+
+
+        // private methods instead of visiting Modifier Contexts:
+
+        private string ProcessModifier(ModifierContext modifierCtx)
+        {
+            if (modifierCtx.classOrInterfaceModifier() is { } classOrInterfaceModCtx)
+                return this.ProcessClassOrInterfaceModifier(classOrInterfaceModCtx);
+
+            return modifierCtx.GetText();
+        }
+
+        private string ProcessClassOrInterfaceModifier([NotNull] ClassOrInterfaceModifierContext classOrInterfaceModCtx)
+        {
+            if (classOrInterfaceModCtx.annotation() is { })
+                throw new NotImplementedException("annotations");
+
+            return classOrInterfaceModCtx.GetText();
+        }
+
+        private string ProcessInterfaceMethodModifier([NotNull] InterfaceMethodModifierContext interfaceMethodModCtx)
+        {
+            if (interfaceMethodModCtx.annotation() is { })
+                throw new NotImplementedException("annotations");
+
+            return interfaceMethodModCtx.GetText();
+        }
+
+        private string ProcessVariableModifier(VariableModifierContext variableModifierCtx)
+        {
+            if (variableModifierCtx.annotation() is { })
+                throw new NotImplementedException("annotations");
+
+            return variableModifierCtx.GetText();
+        }
+
 
         // other (overriden just for the purposes of testing the above methods):
 
@@ -39,6 +222,20 @@ namespace LINVAST.Imperative.Builders.Java
             => new IdNode(ctx.Start.Line,
                 string.Join('.', ctx.IDENTIFIER().Select(id => id.GetText())));
 
+        public override ASTNode VisitClassBody([NotNull] ClassBodyContext ctx)
+            => new BlockStatNode(ctx.Start.Line);
+
+        public override ASTNode VisitInterfaceBody([NotNull] InterfaceBodyContext ctx)
+            => new BlockStatNode(ctx.Start.Line);
+
+        public override ASTNode VisitTypeType([NotNull] TypeTypeContext ctx)
+            => new TypeNameNode(ctx.Start.Line, "String");
+
+        public override ASTNode VisitTypeTypeOrVoid([NotNull] TypeTypeOrVoidContext ctx)
+            => new TypeNameNode(ctx.Start.Line, "String");
+
+        public override ASTNode VisitTypeParameters([NotNull] TypeParametersContext ctx)
+            => new TypeNameListNode(ctx.Start.Line, new TypeNameNode(ctx.Start.Line, "Class1"));
 
     }
 }
---
+++ LINVAST.Tests/Imperative/Builders/Java/EnumDeclarationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LINVAST.Imperative.Builders.Java;
+using LINVAST.Imperative.Nodes;
+using LINVAST.Nodes;
+using LINVAST.Tests.Imperative.Builders.Common;
+using NUnit.Framework;
+
+namespace LINVAST.Tests.Imperative.Builders.Java
+{
+    internal sealed class EnumDeclarationTests : DeclarationTestsBase
+    {
+        
+        [Test]
+        public void NoConstantsEnumDeclTest()
+        {
+            string src1 = "enum Color {}";
+            EnumDeclNode ast1 = this.GenerateAST(src1).As<EnumDeclNode>();
+            Assert.That(ast1.Identifier, Is.EqualTo("Color"));
+            Assert.That(ast1.GetText(), Is.EqualTo("Color"));
+        }
+
+        [Test]
+        public void ImplementsInterfaceEnumDeclTest()
+        {
+            string src1 = "enum Color implements Name1 {}";
+            Assert.That(() => this.GenerateAST(src1), Throws.InstanceOf<NotImplementedException>());
+        }
+
+
+        protected override ASTNode GenerateAST(string src) 
+            => new JavaASTBuilder().BuildFromSource(src, p => p.enumDeclaration());
+    }
+}
