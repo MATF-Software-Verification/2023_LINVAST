05785406b20adb34b885376aa16cd7a1745db8c2
jkeljac <mi18106@alas.matf.bg.ac.rs>
2023-08-29 21:11:07 +0200

    implement declaration and type visitors

--- LINVAST.Imperative/Builders/Go/GoASTBuilder.Declarations.cs
+++ LINVAST.Imperative/Builders/Go/GoASTBuilder.Declarations.cs
@@ -1,34 +1,121 @@
 ﻿using LINVAST.Builders;
+using LINVAST.Imperative.Nodes;
 using LINVAST.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace LINVAST.Imperative.Builders.Go
 {
     public sealed partial class GoASTBuilder : GoParserBaseVisitor<ASTNode>, IASTBuilder<GoParser>
     {
-        public override ASTNode VisitPackageClause(GoParser.PackageClauseContext context) => base.VisitPackageClause(context);
-        
-        public override ASTNode VisitDeclaration(GoParser.DeclarationContext context) => base.VisitDeclaration(context);
+        public override ASTNode VisitPackageClause(GoParser.PackageClauseContext context) => throw new NotImplementedException("Packages");
 
-        public override ASTNode VisitConstDecl(GoParser.ConstDeclContext context) => base.VisitConstDecl(context);
+        public override ASTNode VisitDeclaration(GoParser.DeclarationContext context) => this.Visit(context.children.Single());
 
-        public override ASTNode VisitFieldDecl(GoParser.FieldDeclContext context) => base.VisitFieldDecl(context);
+        public override ASTNode VisitIdentifierList(GoParser.IdentifierListContext context) => new IdListNode(context.Start.Line, context.IDENTIFIER().Select(e => new IdNode(context.Start.Line, e.GetText())));
 
-        public override ASTNode VisitImportDecl(GoParser.ImportDeclContext context) => base.VisitImportDecl(context);
+        public override ASTNode VisitImportDecl(GoParser.ImportDeclContext context) => new ImportListNode(context.Start.Line, context.importSpec().Select(i => this.Visit(i).As<ImportNode>()));
+
+        public override ASTNode VisitImportSpec(GoParser.ImportSpecContext context)
+        {
+            var importPath = new ImportNode(context.Start.Line, this.Visit(context.importPath()).As<IdNode>().Identifier);
+
+
+            if (context.DOT() is null && context.IDENTIFIER() is null) {
+                return importPath;
+            }
+
+            if (context.DOT() is not null) {
+                return new ImportNode(context.Start.Line, ". " + importPath.Directive);
+            }
+
+            if (context.IDENTIFIER() is not null) {
+                return new ImportNode(context.Start.Line, context.IDENTIFIER().GetText() + " " + importPath.Directive);
+            }
+
+            throw new NotSupportedException("Invalid import");
+        }
+
+        public override ASTNode VisitImportPath(GoParser.ImportPathContext context) => new IdNode(context.Start.Line, context.string_().GetText());
+
+        public override ASTNode VisitVarDecl(GoParser.VarDeclContext context)
+        {
+            if (context.varSpec().Count() == 1) {
+                return this.Visit(context.varSpec().First()).As<DeclStatNode>();
+            } else {
+                return new BlockStatNode(context.Start.Line, context.varSpec().Select(vs => this.Visit(vs).As<DeclStatNode>()));
+            }
+        }
+
+        public override ASTNode VisitVarSpec(GoParser.VarSpecContext context)
+        {
+            IdListNode idListNodes = this.Visit(context.identifierList()).As<IdListNode>();
+            DeclListNode d;
+            IEnumerable<VarDeclNode> idExprList;
+            DeclSpecsNode type;
+
+            if (context.type_() is not null) {
+                type = new DeclSpecsNode(context.Start.Line, context.type_().GetText());
+            } else {
+                type = new DeclSpecsNode(context.Start.Line, "");
+            }
+
+            if (context.expressionList() is not null) {
+                ExprListNode exprList = this.Visit(context.expressionList()).As<ExprListNode>();
+
+                idExprList = idListNodes.Identifiers.Zip(exprList.Expressions, (i, e) => new VarDeclNode(context.Start.Line, i, e));
+            } else {
+                idExprList = idListNodes.Identifiers.Select(i => new VarDeclNode(context.Start.Line, i));
+            }
+
+            d = new DeclListNode(context.Start.Line, idExprList);
+            return new DeclStatNode(context.Start.Line, type, d);
+        }
+
+        public override ASTNode VisitShortVarDecl(GoParser.ShortVarDeclContext context)
+        {
+            IdListNode idListNodes = this.Visit(context.identifierList()).As<IdListNode>();
+            ExprListNode exprList = this.Visit(context.expressionList()).As<ExprListNode>();
+
+            IEnumerable<VarDeclNode> idExprList = idListNodes.Identifiers.Zip(exprList.Expressions, (i, e) => new VarDeclNode(context.Start.Line, i, e));
+            return new DeclListNode(context.Start.Line, idExprList);
+        }
+
+        public override ASTNode VisitConstSpec(GoParser.ConstSpecContext context)
+        {
+            IdListNode idListNodes = this.Visit(context.identifierList()).As<IdListNode>();
+            DeclListNode d;
+            IEnumerable<VarDeclNode> idExprList;
+            DeclSpecsNode type;
+
+            if (context.type_() is not null) {
+                type = new DeclSpecsNode(context.Start.Line, "const", context.type_().GetText());
+            } else {
+                type = new DeclSpecsNode(context.Start.Line, "const", "");
+            }
+
+            if (context.expressionList() is not null) {
+                ExprListNode exprList = this.Visit(context.expressionList()).As<ExprListNode>();
+                idExprList = idListNodes.Identifiers.Zip(exprList.Expressions, (i, e) => new VarDeclNode(context.Start.Line, i, e));
+            } else {
+                idExprList = idListNodes.Identifiers.Select(i => new VarDeclNode(context.Start.Line, i));
+            }
+
+            d = new DeclListNode(context.Start.Line, idExprList);
+            return new DeclStatNode(context.Start.Line, type, d);
+        }
+
+        public override ASTNode VisitConstDecl(GoParser.ConstDeclContext context)
+        {
+            if (context.constSpec().Count() == 1) {
+                return this.Visit(context.constSpec().First()).As<DeclStatNode>();
+            } else {
+                return new BlockStatNode(context.Start.Line, context.constSpec().Select(cs => this.Visit(cs).As<DeclStatNode>()));
+            }
+        }
 
         public override ASTNode VisitTypeDecl(GoParser.TypeDeclContext context) => base.VisitTypeDecl(context);
-
-        public override ASTNode VisitVarDecl(GoParser.VarDeclContext context) => base.VisitVarDecl(context);
-
-        public override ASTNode VisitShortVarDecl(GoParser.ShortVarDeclContext context) => base.VisitShortVarDecl(context);
-
-        public override ASTNode VisitConstSpec(GoParser.ConstSpecContext context) => base.VisitConstSpec(context);
-
-        public override ASTNode VisitIdentifierList(GoParser.IdentifierListContext context) => base.VisitIdentifierList(context);
-
-        public override ASTNode VisitVarSpec(GoParser.VarSpecContext context) => base.VisitVarSpec(context);
-
-        public override ASTNode VisitImportPath(GoParser.ImportPathContext context) => base.VisitImportPath(context);
-
-        public override ASTNode VisitImportSpec(GoParser.ImportSpecContext context) => base.VisitImportSpec(context);
     }
 }
--- LINVAST.Imperative/Builders/Go/GoASTBuilder.Types.cs
+++ LINVAST.Imperative/Builders/Go/GoASTBuilder.Types.cs
@@ -1,44 +1,125 @@
-﻿using LINVAST.Builders;
+﻿using System;
+using LINVAST.Builders;
+using LINVAST.Imperative.Nodes;
 using LINVAST.Nodes;
+using System.Linq;
+
 
 namespace LINVAST.Imperative.Builders.Go
 {
     public sealed partial class GoASTBuilder : GoParserBaseVisitor<ASTNode>, IASTBuilder<GoParser>
     {
-        public override ASTNode VisitType_(GoParser.Type_Context context) => base.VisitType_(context);
+        public override ASTNode VisitType_(GoParser.Type_Context context)
+        {
+            if (context.typeName() is not null) {
+                return this.Visit(context.typeName()).As<TypeNameNode>();
+            }
+            if (context.typeLit() is not null) {
+                return this.Visit(context.typeLit()).As<TypeNameNode>();
+            }
 
-        public override ASTNode VisitTypeName(GoParser.TypeNameContext context) => base.VisitTypeName(context);
+            return this.Visit(context.type_()).As<TypeNameNode>();
+        }
 
-        public override ASTNode VisitTypeLit(GoParser.TypeLitContext context) => base.VisitTypeLit(context);
+        public override ASTNode VisitTypeName(GoParser.TypeNameContext context)
+        {
+            if (context.qualifiedIdent() is not null) {
+                return this.Visit(context.qualifiedIdent()).As<TypeNameNode>();
+            }
+            string name = context.IDENTIFIER().GetText();
+            return new TypeNameNode(context.Start.Line, name);
+        }
 
-        public override ASTNode VisitTypeList(GoParser.TypeListContext context) => base.VisitTypeList(context);
+        public override ASTNode VisitTypeLit(GoParser.TypeLitContext context) => this.Visit(context.children.Single());
 
-        public override ASTNode VisitArrayType(GoParser.ArrayTypeContext context) => base.VisitArrayType(context);
+        //TODO
+        public override ASTNode VisitTypeList(GoParser.TypeListContext context)
+        {
+            if (context.type_() is not null) {
+                return new TypeNameListNode(context.Start.Line, context.type_().Select(t => this.Visit(t).As<TypeNameNode>()));
+            };
 
-        public override ASTNode VisitArrayLength(GoParser.ArrayLengthContext context) => base.VisitArrayLength(context);
+            return new TypeNameListNode(context.Start.Line);
+        }
 
-        public override ASTNode VisitStructType(GoParser.StructTypeContext context) => base.VisitStructType(context);
+        public override ASTNode VisitNonNamedType(GoParser.NonNamedTypeContext context)
+        {
+            if (context.typeLit() is not null) {
+                return this.Visit(context.typeLit()).As<TypeNameNode>();
+            }
 
-        public override ASTNode VisitPointerType(GoParser.PointerTypeContext context) => base.VisitPointerType(context);
+            return this.Visit(context.nonNamedType()).As<TypeNameNode>();
+        }
 
-        public override ASTNode VisitResult(GoParser.ResultContext context) => base.VisitResult(context);
+        public override ASTNode VisitArrayLength(GoParser.ArrayLengthContext context) => this.Visit(context.expression()).As<ExprNode>();
 
-        public override ASTNode VisitFunctionType(GoParser.FunctionTypeContext context) => base.VisitFunctionType(context);
 
-        public override ASTNode VisitInterfaceType(GoParser.InterfaceTypeContext context) => base.VisitInterfaceType(context);
+        /*public override ASTNode VisitArrayType(GoParser.ArrayTypeContext context)
+        {
+            ExprStatNode length = this.Visit(context.arrayLength()).As<ExprStatNode>();
+            TypeNode elem = this.Visit(context.elementType()).As<TypeNode>();
+            throw new NotImplementedException("arrayType");
+        }*/
 
-        public override ASTNode VisitSliceType(GoParser.SliceTypeContext context) => base.VisitSliceType(context);
 
-        public override ASTNode VisitMapType(GoParser.MapTypeContext context) => base.VisitMapType(context);
+        /*public override ASTNode VisitStructType(GoParser.StructTypeContext context)
+        {
+            throw new Exception("struct");
 
-        public override ASTNode VisitChannelType(GoParser.ChannelTypeContext context) => base.VisitChannelType(context);
+        }*/
 
-        public override ASTNode VisitConversion(GoParser.ConversionContext context) => base.VisitConversion(context);
+        /*public override ASTNode VisitPointerType(GoParser.PointerTypeContext context)
+        {
+            return this.Visit(context.type_()).As<TypeNode>();
+        }*/
 
-        public override ASTNode VisitEmbeddedField(GoParser.EmbeddedFieldContext context) => base.VisitEmbeddedField(context);
+        public override ASTNode VisitResult(GoParser.ResultContext context)
+        {
+            if (context.parameters() is not null) {
+                return this.Visit(context.parameters());
+            }
+            return this.Visit(context.type_());
+        }
 
-        public override ASTNode VisitTypeSpec(GoParser.TypeSpecContext context) => base.VisitTypeSpec(context);
+        //public override ASTNode VisitFunctionType(GoParser.FunctionTypeContext context) => this.Visit(context.signature());
 
-        public override ASTNode VisitNonNamedType(GoParser.NonNamedTypeContext context) => base.VisitNonNamedType(context);
+        /*public override ASTNode VisitInterfaceType(GoParser.InterfaceTypeContext context)
+        {
+
+            //return new InterfaceNode(context.Start.Line, )
+            throw new NotImplementedException("interfaceType");
+        }*/
+
+        //public override ASTNode VisitSliceType(GoParser.SliceTypeContext context) => this.Visit(context.elementType()).As<TypeNode>();
+
+        /*public override ASTNode VisitMapType(GoParser.MapTypeContext context)
+        {
+            TypeNode type = this.Visit(context.type_()).As<TypeNode>();
+            TypeNode elem = this.Visit(context.elementType()).As<TypeNode>();
+
+            throw new NotImplementedException("Map");
+
+        }*/
+        //public override ASTNode VisitChannelType(GoParser.ChannelTypeContext context) => this.Visit(context.elementType()).As<TypeNode>();
+
+        /*public override ASTNode VisitConversion(GoParser.ConversionContext context)
+        {
+            TypeNode type = this.Visit(context.nonNamedType()).As<TypeNameNode>();
+            throw new NotImplementedException("conversion");
+        }*/
+
+        //jok
+        //public override ASTNode VisitEmbeddedField(GoParser.EmbeddedFieldContext context) => base.VisitEmbeddedField(context);
+        //public override ASTNode VisitFieldDecl(GoParser.FieldDeclContext context) => base.VisitFieldDecl(context);
+
+        /*public override ASTNode VisitTypeSpec(GoParser.TypeSpecContext context)
+        {
+
+            string name = context.IDENTIFIER().GetText();
+            this.Visit(context.type_()).As<TypeNode>();
+
+            throw new NotImplementedException("typespec");
+
+        }*/
     }
 }
